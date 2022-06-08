068071a3704e573c1cd3eeab9dd21ac79e311df3
Igor Bojczuk <nibojczuk@gmail.com>
2022-06-08 11:37:11 +0200

    port fix

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.get("/invalidate/:username", (req, res) => {
   }
 });
 
-var server = app.listen(3000, () => {
+var server = app.listen(process.env.port || 3000, () => {
   var host = server.address().address;
   var port = server.address().port;
   console.log("Example app listening at http://%s:%s", host, port);
